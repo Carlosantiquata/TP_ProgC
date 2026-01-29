577d8ef8afc03e90aa4651e7aa0fc8ebcde7339e
carlosantiquata <a.terrao-beca@cfacampusmontsouris.fr>
2026-01-29 10:32:59 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    printf("a + b = %d\n", a + b); // Addition
+    printf("a - b = %d\n", a - b); // Soustraction
+    printf("a * b = %d\n", a * b); // Multiplication
+    printf("a / b = %d\n", a / b); // Division (entière)
+    printf("a %% b = %d\n", a % b); // Modulo (le reste)
+
+    // Pour les comparaisons : 1 = VRAI, 0 = FAUX
+    printf("a == b ? %d\n", a == b); // Est-ce égal ?
+    printf("a > b  ? %d\n", a > b);  // Est-ce plus grand ?
+
+    return 0;
+}
 
 
