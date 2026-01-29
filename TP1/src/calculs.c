7a5541a1b7c8314ca75194bb16449b380615cb7d
carlosantiquata <a.terrao-beca@cfacampusmontsouris.fr>
2026-01-29 11:49:16 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 12;
+    int num2 = 4;
+    char op = '+'; // Change ce signe pour tester (+, -, *, /, %, &, |, ~)
+
+    switch (op) {
+        case '+': printf("%d\n", num1 + num2); break;
+        case '-': printf("%d\n", num1 - num2); break;
+        case '*': printf("%d\n", num1 * num2); break;
+        case '/': printf("%d\n", num1 / num2); break;
+        case '%': printf("%d\n", num1 % num2); break;
+        case '&': printf("%d\n", num1 & num2); break;
+        case '|': printf("%d\n", num1 | num2); break;
+        case '~': printf("%d\n", ~num1); break;
+        default: printf("Erreur\n");
+    }
+
+    return 0;
+}
 
